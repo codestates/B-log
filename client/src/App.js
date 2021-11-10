de366d95e1af29100c293b5a3038b3ce132669a2
yeinMOON <yeindianamoon@gmail.com>
2021-11-11 00:23:13 +0900

    Add props to Header component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import EditPassword from "./pages/EditPassword";
 import LogIn from "./pages/LogIn";
-import axios from "axios";
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
 import Search from "./pages/Search";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header isLogin={isLogin} setIsLogin={setIsLogin} />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mypage" element={<Redirect />} />
