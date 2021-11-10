02dd67b30ac0bce84b35c12b214cc8229b98cd41
Gyosic <zpzptls@naver.com>
2021-11-11 00:23:29 +0900

    Add App.css & Fix App.js, SignUp.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import EditPassword from "./pages/EditPassword";
 import LogIn from "./pages/LogIn";
-import axios from "axios";
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
 import Search from "./pages/Search";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
