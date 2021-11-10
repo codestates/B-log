d72df12773a79fffbf71787e5eb845c921f53a51
수민 N <suminn@sumin-ui-MacBookAir.local>
2021-11-11 00:37:29 +0900

    Add pages column to Book table

--- server/migrations/20211110023349-create-book.js
+++ server/migrations/20211110023349-create-book.js
@@ -26,6 +26,9 @@ module.exports = {
       isbn13: {
         type: Sequelize.STRING
       },
+      pages: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
