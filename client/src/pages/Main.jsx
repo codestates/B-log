f654ac4ff0d38294d0ce42f66cceb225fd64de52
yeinMOON <yeindianamoon@gmail.com>
2021-11-17 02:06:09 +0900

    Add recently add section & request function

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { updateRack, updateShelf, loginStateChange } from "../actions/index";
+import {
+  updateRack,
+  updateShelf,
+  getSearchKeyword,
+  loginStateChange,
+  getSearchResult,
+} from "../actions/index";
 import styled from "styled-components";
 import SearchInput from "../components/SearchInput";
 import BookGrid from "../components/BookGrid";
@@ -8,6 +14,9 @@ import BookInfoModal from "../components/BookInfoModal";
 import BookMarkModal from "../components/BookMarkModal";
 import Footer from "../components/Footer";
 import Qwigley from "../assets/fonts/Qwigley-Regular.woff";
+// import Poppins from "../assets/fonts/Poppins-ExtraLightItalic.woff";
+// import Poppins from "../assets/fonts/Poppins-Light.woff";
+import Poppins from "../assets/fonts/Poppins-ExtraLight.woff";
 import axios from "axios";
 
 import { noTop10 } from "../assets/dummy/noResponse";
@@ -23,18 +32,31 @@ const Wrapper = styled.div`
   .grid_container {
     padding: 40px;
   }
+
+  .title {
+    color: #443128;
+    font-size: 18px;
+    font-weight: 400;
+    margin: 15px 0;
+    padding-bottom: 5px;
+    border-bottom: 1px solid #e0e0d8;
+  }
 `;
 
 const MainLogo = styled.div`
-  @font-face {
+  /* @font-face {
     font-family: "Qwigley";
     src: local("Qwigley"), url(${Qwigley}) format("woff");
   }
-  font-family: "Qwigley", "cursive";
-  font-size: 130px;
-  font-weight: 400;
+  font-family: "Qwigley", "cursive"; */
+  @font-face {
+    font-family: "Poppins";
+    src: local("Poppins"), url(${Poppins}) format("woff");
+  }
+  font-family: "Poppins", sans-serif;
+  font-size: 100px;
   color: #594d49;
-  margin-bottom: 20px;
+  margin: 70px 0px 30px 0;
   text-shadow: 5px 5px #e0e0d8;
 `;
 
@@ -48,17 +70,21 @@ function Main() {
   const [markOpen, setMarkOpen] = useState(false);
   const [bookinfo, setBookinfo] = useState({});
   const [top10, setTop10] = useState(noTop10);
+  const [new10, setNew10] = useState(noTop10);
   const myBooks = [...rack, ...shelf];
 
   const getTop10 = () => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/books`)
-      .then((res) => {
-        setTop10(res.data.books);
-      })
-      .catch(() => {
-        setTop10(noTop10);
-      });
+      .then((res) => setTop10(res.data.books))
+      .catch(() => setTop10(noTop10));
+  };
+
+  const getPopular = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/books/users`)
+      .then((res) => setNew10(res.data.books))
+      .catch(() => setNew10(noTop10));
   };
 
   const getMyBooks = async () => {
@@ -96,20 +122,35 @@ function Main() {
   useEffect(() => {
     getMyBooks();
     getTop10();
+    getPopular();
+    dispatch(getSearchKeyword(""));
+    dispatch(getSearchResult([]));
     // eslint-disable-next-line
   }, []);
 
   return (
     <>
       <Wrapper>
-        <MainLogo>B-log</MainLogo>
+        <MainLogo>B : log</MainLogo>
         <SearchInput />
         <div className="grid_container">
+          <h3 className="title">새로운 책을 찾고 있나요?</h3>
           <BookGrid
             infoModalHandler={infoModalHandler}
             markModalHandler={markModalHandler}
             myBooks={myBooks}
             books={top10}
+            col={5}
+          />
+        </div>
+        <div className="grid_container">
+          <h3 className="title">다른 회원들은 지금...</h3>
+
+          <BookGrid
+            infoModalHandler={infoModalHandler}
+            markModalHandler={markModalHandler}
+            myBooks={myBooks}
+            books={new10}
             col={5}
           />
         </div>
