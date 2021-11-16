f0c250d771e9127929634d6f093c5aceff6a6019
yeinMOON <yeindianamoon@gmail.com>
2021-11-16 20:48:11 +0900

    Fix details & Fix typo

--- client/src/components/BookGrid.jsx
+++ client/src/components/BookGrid.jsx
@@ -5,7 +5,7 @@ const Container = styled.ul`
   display: grid;
   grid-template-rows: repeat(auto-fill, 1fr);
   grid-template-columns: ${(props) => `repeat(${props.col}, 1fr)`};
-  gap: 30px;
+  gap: 35px;
 `;
 function BookGrid({ infoModalHandler, markModalHandler, myBooks, books, col }) {
   const read = myBooks.map((book) => book.isbn13);
@@ -15,12 +15,12 @@ function BookGrid({ infoModalHandler, markModalHandler, myBooks, books, col }) {
       <Container col={col}>
         {books.map((book, idx) => (
           <Book
+            key={idx}
             infoModalHandler={infoModalHandler}
             markModalHandler={markModalHandler}
             isMypage={false}
             bookinfo={book}
             marked={read.includes(book.isbn13) ? "true" : null}
-            key={idx}
           />
         ))}
       </Container>
