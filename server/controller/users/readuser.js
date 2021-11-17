b3de9184a208383d685c48d0a58fdc543f33caef
수민 N <suminn@sumin-ui-MacBookAir.local>
2021-11-17 12:23:51 +0900

    Add auth middleware to remove redundancy

--- server/controller/users/readuser.js
+++ server/controller/users/readuser.js
@@ -4,13 +4,10 @@ module.exports = {
   get: (req, res) => {
     try {
       const userinfo = token.isAuthorized(req);
-      if (!userinfo) {
-        return res.status(401).send({ message: "invalid access token" });
-      } else {
-        delete userinfo.iat;
-        delete userinfo.exp;
-        res.send(userinfo);
-      }
+
+      delete userinfo.iat;
+      delete userinfo.exp;
+      res.send(userinfo);
     } catch {
       res.status(500).send();
     }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { usersController } = require("../controller");
+const checkAuth = require("../middleware/checkAuth");
 
-router.get("/", usersController.readuser.get);
+router.get("/", checkAuth, usersController.readuser.get);
 
-router.patch("/password", usersController.password.patch);
+router.patch("/password", checkAuth, usersController.password.patch);
 
-router.patch("/username", usersController.username.patch);
+router.patch("/username", checkAuth, usersController.username.patch);
 
 module.exports = router;
