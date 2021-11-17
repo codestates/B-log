0726bf47e97d629192d34574e4044450fb2a09a2
SoominNa <85681803+soominna@users.noreply.github.com>
2021-11-17 14:19:46 +0900

    Add auth verification middleware
    * Add auth middleware to remove redundancy
    
    * Fix sequelize query in mypage controller

--- server/controller/myPage/review.js
+++ server/controller/myPage/review.js
@@ -6,25 +6,20 @@ module.exports = {
     //헤더 토큰 해독해서 로그인한 유저의 id 가져오기
     const user = token.isAuthorized(req);
 
-    //토큰이 없거나 유효하지 않음 401 코드 응답
-    if (!user) {
-      res.status(401).send({ message: "access token expired" });
-    } else {
-      try {
-        const bookId = req.params.bookid;
-        Shelf.findOne({
-          where: { bookId: bookId, userId: user.id, isDoneReading: true },
+    try {
+      const bookId = req.params.bookid;
+      Shelf.findOne({
+        where: { bookId: bookId, userId: user.id, isDoneReading: true },
+      })
+        .then((shelfBook) => {
+          console.log(shelfBook.dataValues);
+          res.status(200).send({ data: shelfBook.dataValues.review || "" });
         })
-          .then((shelfBook) => {
-            console.log(shelfBook.dataValues);
-            res.status(200).send({ data: shelfBook.dataValues.review || "" });
-          })
-          .catch((err) => {
-            res.status(404).send({ message: "Resource not found" });
-          });
-      } catch {
-        res.status(500).send();
-      }
+        .catch((err) => {
+          res.status(404).send({ message: "Resource not found" });
+        });
+    } catch {
+      res.status(500).send();
     }
   },
 
@@ -32,60 +27,52 @@ module.exports = {
     //헤더 토큰 해독해서 로그인한 유저의 id 가져오기
     const user = token.isAuthorized(req);
 
-    if (!user) {
-      res.status(401).send({ message: "access token expired" });
-    } else {
-      try {
-        const bookId = req.params.bookid;
-        Shelf.update(
-          {
-            review: req.body.review,
-          },
-          {
-            where: { userId: user.id, bookId: bookId, isDoneReading: true },
-          }
-        )
-          .then((updated) => {
-            if (updated[0]) {
-              res.send({ data: req.body.review, message: "updated" });
-            } else {
-              res.status(404).send({ message: "Resource not found" });
-            }
-          })
-          .catch((err) => {
+    try {
+      const bookId = req.params.bookid;
+      Shelf.update(
+        {
+          review: req.body.review,
+        },
+        {
+          where: { userId: user.id, bookId: bookId, isDoneReading: true },
+        }
+      )
+        .then((updated) => {
+          if (updated[0]) {
+            res.send({ data: req.body.review, message: "updated" });
+          } else {
             res.status(404).send({ message: "Resource not found" });
-          });
-      } catch {
-        res.status(500).send();
-      }
+          }
+        })
+        .catch((err) => {
+          res.status(404).send({ message: "Resource not found" });
+        });
+    } catch {
+      res.status(500).send();
     }
   },
 
   delete: (req, res) => {
     const user = token.isAuthorized(req);
 
-    if (!user) {
-      res.status(401).send({ message: "access token expired" });
-    } else {
-      try {
-        const bookId = req.params.bookid;
-        Shelf.update(
-          {
-            review: "",
-          },
-          {
-            where: { userId: user.id, bookId: bookId, isDoneReading: true },
-          }
-        )
-          .then((deleted) => {
-            res.status(204).send();
-          })
-          .catch((err) => {
-            res.status(404).send({ message: "Resource not found" });
-          });
-      } catch {
-        res.status(500).send();
-      }
+    try {
+      const bookId = req.params.bookid;
+      Shelf.update(
+        {
+          review: "",
+        },
+        {
+          where: { userId: user.id, bookId: bookId, isDoneReading: true },
+        }
+      )
+        .then((deleted) => {
+          res.status(204).send();
+        })
+        .catch((err) => {
+          res.status(404).send({ message: "Resource not found" });
+        });
+    } catch {
+      res.status(500).send();
     }
   },
 };
--- server/controller/users/username.js
+++ server/controller/users/username.js
@@ -4,35 +4,32 @@ const { token } = require("../serverFunctions");
 module.exports = {
   patch: (req, res) => {
     const userinfo = token.isAuthorized(req);
-    if (!userinfo) {
-      res.status(401).send({ message: "invalid access token" });
-    } else {
-      if (req.body.username && req.body.username !== "") {
-        User.update(
-          { username: req.body.username },
-          { where: { id: userinfo.id } }
-        )
-          .then((data) => {
-            const accessToken = token.generateAccessToken({
-              id: userinfo.id,
-              username: req.body.username,
-              email: userinfo.email,
-            });
-            token.sendAccessToken(res, accessToken);
-            res.send({
-              username: req.body.username,
-              message: "ok",
-            });
-          })
-          .catch((err) => {
-            res.status(500).send("Server err");
+
+    if (req.body.username && req.body.username !== "") {
+      User.update(
+        { username: req.body.username },
+        { where: { id: userinfo.id } }
+      )
+        .then((data) => {
+          const accessToken = token.generateAccessToken({
+            id: userinfo.id,
+            username: req.body.username,
+            email: userinfo.email,
           });
-      } else {
-        res.send({
-          username: userinfo.username,
-          message: "ok",
+          token.sendAccessToken(res, accessToken);
+          res.send({
+            username: req.body.username,
+            message: "ok",
+          });
+        })
+        .catch((err) => {
+          res.status(500).send("Server err");
         });
-      }
+    } else {
+      res.send({
+        username: userinfo.username,
+        message: "ok",
+      });
     }
   },
 };
